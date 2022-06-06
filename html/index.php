541d312ec4a08b899ee82bdcbb971a2e89d7e954
masaya-nishimura-lvgs <masaya.nishimura@leverages.jp>
2022-06-06 18:32:59 +0900

    DB保存処理完了

--- html/index.php
+++ html/index.php
@@ -10,6 +10,9 @@
 
 <?php 
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 require __DIR__ . '/my-sample-project/vendor/autoload.php';
 
 $key = __DIR__ . '/my-sample-project/river-psyche-352208-8071edae4752.json';
@@ -27,8 +30,33 @@ $sheet = new \Google_Service_Sheets($client);
 $sheet_name = "シート1"; // シートを指定
 $sheet_range = "A2:B8"; // 範囲を指定。開始から終了まで斜めで囲む感じです。
 $response = $sheet->spreadsheets_values->get($sheet_id, $sheet_name.'!'.$sheet_range);
-foreach ($response->getValues() as $index => $cols) {
-    var_dump($cols);
+
+try {
+    //データベース名、ユーザー名、パスワード
+    $dsn = 'mysql:dbname=test;host=c568cfe25d54;charset=utf8';
+    $user = 'test';
+    $password = 'test';
+    //MySQLのデータベースに接続
+    $pdo = new PDO($dsn, $user, $password);
+    //PDOのエラーレポートを表示
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    foreach ($response->getValues() as $index => $cols) {
+        // INSERT文を変数に格納
+        // あらかじめMySQL内にテーブルとカラムを作成しておく必要がある
+        $sql = "INSERT INTO users (name, email) VALUES (:name, :email)";
+        //挿入する値は空のまま、SQL実行の準備をする
+        $stmt = $pdo->prepare($sql);
+        // 挿入する値を配列に格納する
+        $params = array(':name' => $cols[0], ':email' => $cols[1]);
+        //挿入する値が入った変数をexecuteにセットしてSQLを実行
+        $stmt->execute($params);
+    }
+
+    echo '登録しました！';
+
+} catch (PDOException $e) {
+    exit('データベースに接続できませんでした。' . $e->getMessage());
 }
 
 ?>
